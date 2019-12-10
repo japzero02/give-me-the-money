1ea592267e3ba9be52aaf0cc40fc7c757ea0acbc
Pornpattara <zPornpattaraK@pttep.com>
2019-12-10 14:16:01 +0700

    Add file edit_item and create function edit_student in view.py for edit student table after booking

--- money/mainapp/views.py
+++ money/mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .forms import *
 
@@ -28,3 +28,16 @@ def add_student(request):
     else:
         form = StudentForm()
         return render(request, 'add_new.html', {'form': form})
+
+
+def edit_student(request, pk):
+    """edit student by primarykey"""
+    item = get_object_or_404(Student, pk=pk)
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = StudentForm(instance=item)
+        return render(request, 'edit_item.html', {'form': form})
