789924e7d11f6733f8106cf42074fd42c24ba7ff
Pornpattara <zPornpattaraK@pttep.com>
2019-12-11 11:46:52 +0700

    add fucntion delete_student in views.py and add delete url in urls.py

--- money/mainapp/urls.py
+++ money/mainapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^display_student$', display_student, name='display_student'),
     url(r'^add_student$', add_student, name="add_student"),
     url(r'^edit_student/(?P<pk>\d+)$', edit_student, name='edit_student'),
+    url(r'^delete_student/(?P<pk>\d+)$', delete_student, name='delete_student'),
 ]
