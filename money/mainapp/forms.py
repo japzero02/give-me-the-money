ea02f1e2ac4fe86fef1c239189566207f19d98b6
it59070097 <59070097@it.kmitl.ac.th>
2019-12-12 16:17:24 +0700

    change design

--- money/mainapp/forms.py
+++ money/mainapp/forms.py
@@ -8,3 +8,22 @@ class StudentForm(forms.ModelForm):
                   'february', 'march', 'april', 'may',
                   'june', 'july', 'august', 'september',
                   'october', 'november', 'december', 'issues')
+        widgets = {
+            'student_id': forms.TextInput(attrs={'class':'form-control'}),
+            'first_name': forms.TextInput(attrs={'class':'form-control'}),
+            'last_name': forms.TextInput(attrs={'class':'form-control'}),
+            'january': forms.Select(attrs={'class':'form-control'}),
+            'february': forms.Select(attrs={'class':'form-control'}),
+            'march': forms.Select(attrs={'class':'form-control'}),
+            'april': forms.Select(attrs={'class':'form-control'}),
+            'may': forms.Select(attrs={'class':'form-control'}),
+            'june': forms.Select(attrs={'class':'form-control'}),
+            'july': forms.Select(attrs={'class':'form-control'}),
+            'august': forms.Select(attrs={'class':'form-control'}),
+            'september': forms.Select(attrs={'class':'form-control'}),
+            'october': forms.Select(attrs={'class':'form-control'}),
+            'november': forms.Select(attrs={'class':'form-control'}),
+            'december': forms.Select(attrs={'class':'form-control'}),
+            'issues': forms.TextInput(attrs={'class':'form-control'}),
+        }
+        
